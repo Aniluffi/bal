b33e11eda3eb5fe3d77c8ae5c9f62ff0dac82f13
Aniluffi <90216018+Aniluffi@users.noreply.github.com>
2024-03-29 09:47:11 +0200

    Add files via upload

---
+++ балаш пр2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program 
+{
+    static void Main(string[] args)
+    {
+        int[] array = new int[] {3,4,6,8,9,10,11,23,54,60,70,80,90,100 };//отсортированый массив
+
+        foreach (int el in array) // вывод отсортированого массива
+        {
+            Console.WriteLine(el);
+        }
+
+        Console.WriteLine();
+
+        int search = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0;i < array.Length;i++)
+        {
+            if (array[i] == search)
+            {
+                Console.WriteLine("Yes");
+                break;
+            } else if (i == array.Length - 1)
+            {
+                Console.WriteLine("No");
+            }
+        }
+        
+    }
+
+}
---
+++ балаш3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+class Program
+{
+    static int[] A = new int[1001]; // Array length increased to 1001 to match Pascal array index
+    static int n;
+
+    static void Sl(int l, int s, int r)
+    {
+        int[] B = new int[1001]; // Array length increased to 1001 to match Pascal array index
+        int i, j, k;
+
+        k = l;
+        i = l;
+        j = s;
+
+        Array.Copy(A, B, A.Length);
+
+        while (i <= s && j <= r)
+        {
+            if (A[i] < A[j])
+            {
+                B[k] = A[i];
+                i++;
+            }
+            else
+            {
+                B[k] = A[j];
+                j++;
+            }
+            k++;
+        }
+
+        if (i > s)
+        {
+            for (i = j; i <= r; i++)
+            {
+                B[k] = A[i];
+                k++;
+            }
+        }
+        else
+        {
+            for (j = i; j <= s; j++)
+            {
+                B[k] = A[j];
+                k++;
+            }
+        }
+
+        Array.Copy(B, A, B.Length);
+    }
+
+    static void SortSl(int l, int r)
+    {
+        int s;
+
+        if (l < r)
+        {
+            s = (l + r) / 2;
+            SortSl(l, s);
+            SortSl(s + 1, r);
+            Sl(l, s, r);
+        }
+    }
+
+    static void Main()
+    {
+        // основная программа
+        // ...
+
+        SortSl(1, n);
+        // ...
+
+    }
+}
